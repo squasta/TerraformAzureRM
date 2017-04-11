fec1e0c44e7774411375fec22076c1ca4d553d47
Stanislas Quastana <squastana@hotmail.com>
2017-04-11 16:27:13 +0200

    Update InstallAnsibleRedHat.sh

--- InstallAnsibleRedHat.sh
+++ InstallAnsibleRedHat.sh
@@ -3,10 +3,10 @@
 # subscription-manager register --auto-attach
 
 # The easiest way to install Ansible is by adding a third-party repository named EPEL (Extra Packages for Enterprise Linux), which is maintained over at http://fedoraproject.org/wiki/EPEL. You can easily add the repo by running the following command:
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 # Installation de Ansible
-yum -y install ansible
+sudo yum -y install ansible
 
 # check de la version d Ansible
 ansible --version
