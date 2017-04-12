b2e82e48291ba439dcfdfb528addceb3cca58b11
Stanislas Quastana <squastana@hotmail.com>
2017-04-12 11:54:16 +0200

    Update InstallAnsibleRedHat.sh

--- InstallAnsibleRedHat.sh
+++ InstallAnsibleRedHat.sh
@@ -16,7 +16,7 @@ rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 curl -H "Content-Type: application/json" -d "{\"text\": \"yum -y ansible \"}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
 # Installation de Ansible
-yum -y install ansible
+# yum -y install ansible
 
 # check de la version d Ansible
 # ansible --version
