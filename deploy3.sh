01a2edb204b1641a0da3c24067c6aa15c8a21a61
Stanislas Quastana <squastana@hotmail.com>
2017-04-12 18:41:22 +0200

    Update deploy3.sh

--- deploy3.sh
+++ deploy3.sh
@@ -25,6 +25,6 @@ curl -H "Content-Type: application/json" -d "{\"text\": \"Fin du script \"}" htt
 
 echo "customscript done" > /tmp/results.txt
 
-curl -H "Content-Type: application/json" -d "{\"title\": \"Ouvrir la console Azure \", \"text\": \"Cliquer sur [Acceder au portal Azure](https://portal.azure.com) pour visualiser le resultat \", \"themeColor\": \"EA4300\", \"potentialAction\": [{\"@context\": \"https://schema.org\", \"@type\": \"ViewAction\", \"name\": \"Ouvrir Portail Azure \", \"target\": [\"https://portal.azure.com \"]}]}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
+curl -H "Content-Type: application/json" -d "{\"title\": \"Fin du script sur le bastion \", \"text\": \"Cliquer sur [Acceder au portal Azure](https://portal.azure.com) pour visualiser le resultat \", \"themeColor\": \"EA4300\", \"potentialAction\": [{\"@context\": \"https://schema.org\", \"@type\": \"ViewAction\", \"name\": \"Ouvrir Portail Azure \", \"target\": [\"https://portal.azure.com \"]}]}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
 
 exit 0
