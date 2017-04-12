8b8c821bba6159109352b28c4985778574b793a5
Stanislas Quastana <squastana@hotmail.com>
2017-04-12 12:10:19 +0200

    Update InstallAnsibleRedHat.sh

--- InstallAnsibleRedHat.sh
+++ InstallAnsibleRedHat.sh
@@ -2,14 +2,14 @@
 
 # this is a sample script to use with Azure VM customscript extension to setup Ansible on RHEL 
 
-curl -H "Content-Type: application/json" -d "{\"text\": \"Debut du script InstallAnsibleRedHat.sh \"}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
+# curl -H "Content-Type: application/json" -d "{\"text\": \"Debut du script InstallAnsibleRedHat.sh \"}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
 
 # enregistrement de la VM aupres de Red Hat si besoin
 # subscription-manager register --auto-attach
 
-curl -H "Content-Type: application/json" -d "{\"text\": \"Ajout du depot EPEL \"}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
+# curl -H "Content-Type: application/json" -d "{\"text\": \"Ajout du depot EPEL \"}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
 # The easiest way to install Ansible is by adding a third-party repository named EPEL (Extra Packages for Enterprise Linux), which is maintained over at http://fedoraproject.org/wiki/EPEL. You can easily add the repo by running the following command:
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 # Update si necessaire
 # yum -y update
@@ -23,6 +23,6 @@ rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 echo "customscript done" > /tmp/results.txt
 
-curl -H "Content-Type: application/json" -d "{\"text\": \"Fin du script InstallAnsibleRedHat.sh \"}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
+# curl -H "Content-Type: application/json" -d "{\"text\": \"Fin du script InstallAnsibleRedHat.sh \"}" https://outlook.office.com/webhook/555aa7fc-ea71-4fb7-ae9e-755bba4d04ed@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/16085df23e564bb9876842605ede3af2/51dab674-ad95-4f0a-8964-8bdefc25b6d9
 
 exit 0
