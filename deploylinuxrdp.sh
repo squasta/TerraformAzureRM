12ffda9f70f81caa2b9c45c03d220b37c5c2bac9
Stanislas Quastana <squastana@hotmail.com>
2017-04-28 10:50:07 +0200

    Update deploylinuxrdp.sh

--- deploylinuxrdp.sh
+++ deploylinuxrdp.sh
@@ -4,9 +4,20 @@
 # this is a sample script to check Azure VM customscript extension
 # this script install xubuntu and xrdp
 
-# Installation of curl for logging
-until apt-get -y update && apt-get -y install curl xubuntu-desktop xrdp
+#!/bin/bash
+
+# deployrdplinux.sh
+# this is a sample script to check Azure VM customscript extension
+# this script install xubuntu and xrdp
+
+# Installation of curl for logging, ubuntu-desktop and xubuntu-desktop
+until apt-get -y update && apt-get -y install curl ubuntu-desktop xubuntu-desktop 
 do
   log "Lock detected on VM init Try again..."
   sleep 2
 done
+
+echo xfce4-session >~/.xsession
+
+# sudo vi /etc/xrdp/startwm.sh
+# Add line xfce4-session before the line /etc/X11/Xsession.
