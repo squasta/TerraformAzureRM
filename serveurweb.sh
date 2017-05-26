86a9e5b1079962f1d65125bc09fcc46a233dbe8b
Stanislas Quastana <squastana@hotmail.com>
2017-05-26 15:40:46 +0200

    Update serveurweb.sh

--- serveurweb.sh
+++ serveurweb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello, Azure" > index.hmtl
+echo "Hello, Azure" > index.html
 nohup busybox httpd -f -p 8080 &
 # busybox est un web server par defaut sur Ubuntu
 # nohup et & pour demarrer en background le serveur web
